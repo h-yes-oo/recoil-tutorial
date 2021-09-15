c4b1d24bdf66f6d0b2c6c2e21e71d1ccce5c76df
h-yes-oo <hyesoo5115@naver.com>
2021-09-15 18:11:42 +0900

    todolist - useRecoilState, atom

---
+++ my-app/src/components/Todo.tsx
@@ -0,0 +1,122 @@
+import React, { FC, useState }from 'react';
+import {
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from 'recoil';
+
+interface Todo {
+  id: number;
+  text: string;
+  isComplete: boolean;
+}
+
+const todoListState = atom<Todo[]>({
+  key: 'todoListState',
+  default: [],
+});
+
+let id = 0;
+const getId = () => id++;
+
+// input value로 새로운 todolist item을 만들어 todoList에 추가
+const TodoItemCreator: FC<{}> = () => {
+  const [inputValue, setInputValue] = useState('');
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const addItem = () => {
+    setTodoList((oldTodoList) => [
+      ...oldTodoList,
+      {
+        id: getId(),
+        text: inputValue,
+        isComplete: false,
+      }
+    ]);
+    setInputValue('');
+  }
+
+  const onChange = (event : React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
+    </div>
+  );
+}
+
+function replaceItemAtIndex(arr:any[] , index: number, newValue: any) {
+  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
+
+function removeItemAtIndex(arr: any[], index: number) {
+  return [...arr.slice(0, index), ...arr.slice(index + 1)];
+}
+
+interface TodoItemProps {
+  item: Todo;
+}
+
+const TodoItem: FC<TodoItemProps> = ({ item }) => {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  // 현재 아이템의 인덱스를 찾아서 저장
+  const index = todoList.findIndex((listItem) => listItem === item);
+
+  const editItemText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      text: event.target.value,
+    });
+
+    setTodoList(newList);
+  };
+
+  const toggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isComplete: !item.isComplete,
+    });
+
+    setTodoList(newList);
+  };
+
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(todoList, index);
+
+    setTodoList(newList);
+  };
+
+  return (
+    <div>
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isComplete}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>X</button>
+    </div>
+  );
+}
+
+const TodoList: FC<{}> = () => {
+  const todoList = useRecoilValue(todoListState);
+
+  return (
+    <>
+      {/* <TodoListStats /> */}
+      {/* <TodoListFilters /> */}
+      <TodoItemCreator />
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
+    </>
+  );
+}
+
+export default TodoList;
